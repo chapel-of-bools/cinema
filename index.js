bfe7947acdc4ac724731cf7a1eeb4e141a578774
Matthew Scott <matthewruelscott@gmail.com>
2016-07-26 15:39:34 -0400

    Set up views

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ var mongoose = require("mongoose");
 var Schema = require("./db/schema.js");
 var app = express();
 
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname: ".hbs",
+  partialsDir: "views/",
+  layoutsDir: "views/",
+  defaultLayout: "layout-main"
+}));
+
+app.use("/assets", express.static("public"));
+
 app.get("/", function(req, res){
-  res.send("Hello World!");
+  res.render("app-welcome");
+});
+
+app.get("/theaters", function(req, res){
+  res.render("theaters-index", {
+    numTheaters: 666
+  });
 });
 
 app.listen(3001, function(){
